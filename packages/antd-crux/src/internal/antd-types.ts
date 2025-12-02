c6061b1f2dc1292a2675f6d0a0cbf7b0c49c13fc
Dyrhoi <39205283+Dyrhoi@users.noreply.github.com>
2025-12-02 19:52:54 +0100

    fix type

--- packages/antd-crux/src/internal/antd-types.ts
+++ packages/antd-crux/src/internal/antd-types.ts
@@ -1,4 +1,4 @@
-import type { FormInstance } from "antd";
+import type { FormInstance, Form, GetProps } from "antd";
 export type FieldError = ReturnType<FormInstance["getFieldsError"]>[number];
 export type FieldData = Parameters<FormInstance["setFields"]>[0][number];
-export type { FormListProps } from "antd/es/form";
+export type FormListProps = GetProps<typeof Form.List>;
